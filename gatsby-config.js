25a5102e73f75785de429f7caeb81fa007db5915
Olivier Baptistan <olivierbaptistan@macbook-air-de-olivier.home>
2019-12-12 16:32:56 +0100

    Site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Stones Throw TV`,
+    description: `Stones Throw TV allows you to watch all the music videos made by Stones Throw Records artists. `,
+    author: `Olivier Baptistan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
